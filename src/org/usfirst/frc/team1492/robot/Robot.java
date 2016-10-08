ec232ffa9d731f0c603cb7e523a471fc841086e7
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2016-10-07 20:02:44 -0700

    Fix intake roller out direction

--- src/org/usfirst/frc/team1492/robot/Robot.java
+++ src/org/usfirst/frc/team1492/robot/Robot.java
@@ -413,7 +413,7 @@ public class Robot extends IterativeRobot {
             intake.set(SmartDashboard.getNumber("Intake forward speed", 1));
             break;
         case OUT:
-            intake.set(-SmartDashboard.getNumber("Intake backward speed", 1));
+            intake.set(-SmartDashboard.getNumber("Intake backward speed", -1));
             break;
         }
     }
